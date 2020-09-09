d1e0af6982071d7a09e6a55c5cc824fc599ca699
sinnis <wumengtian1991@gmail.com>
2020-09-09 20:43:14 +0800

    3D models

--- utils.py
+++ utils.py
@@ -19,3 +19,20 @@ def batch_im_save(im,name,path='/content/gdrive/My Drive/model'):
                 sample_im.paste(I1, (x, y))
 
         sample_im.save( os.path.join(path,'sample{}.png'.format(index)) )
+
+def transfrom_batch(im):
+
+    assert np.shape(im)[0] == 64, "This is not a mini-batch(64)"
+
+    im_ = np.zeros((8*128,8*128))
+    
+    for i in range(64):
+        m = i//8
+        n = i%8
+        im_[m*128:(m+1)*128,n*128:(n+1)*128] = im[i]
+
+    for i in range(1,8):
+        im_[i*128,:] = 255
+        im_[:,i*128] = 255
+
+    return im_
