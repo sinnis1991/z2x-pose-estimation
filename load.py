35d04e6873aefcc8fb87626fdb56cf2cf6635286
sinnis <wumengtian1991@gmail.com>
2020-09-14 19:40:28 +0800

    code added

---
+++ load.py
@@ -0,0 +1,12 @@
+def load_example_model(object_index, model_name):
+
+    if object_index == '1':
+
+        #https://drive.google.com/file/d/1wJUWcsITnY3YAyGdUuS54PxH7E3AJIEs/view?usp=sharing
+        !wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies \
+          /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1wJUWcsITnY3YAyGdUuS54PxH7E3AJIEs' \
+          -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1wJUWcsITnY3YAyGdUuS54PxH7E3AJIEs" -O model.zip && rm -rf /tmp/cookies.txt
+
+        !tar -xvf model_1.tar
+
+
