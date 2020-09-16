ee268bc206171cfbe2a33fa4b2d3d7e2a62ce996
sinnis <wumengtian1991@gmail.com>
2020-09-16 19:09:43 +0800

    model added

--- load_model.sh
+++ load_model.sh
@@ -4,6 +4,7 @@ a="model1"
 c="model3"
 d="model4"
 e="model5"
+f="model6"
 
 if [ "$1" == "$a" ]
 then
@@ -14,7 +15,6 @@ wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download
 
 tar -xvf model.tar
 mv ./model_1 $2
-# echo $a
 elif [ "$1" == "$c" ]
 then
 # https://drive.google.com/file/d/1-UdE8ZUe4BVELN86OWLt5jAcCGv-Stsz/view?usp=sharing
@@ -33,8 +33,6 @@ wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download
 
 tar -xvf model.tar
 mv ./model_4 $2
-# echo $c
-# echo $c
 elif [ "$1" == "$e" ]
 then
 # https://drive.google.com/file/d/1h06ozWwQAsyW4IZeZgKXO5tTAUQ1U4Pf/view?usp=sharing
@@ -44,7 +42,15 @@ wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download
 
 tar -xvf model.tar
 mv ./model_5 $2
-# echo $c
+elif [ "$1" == "$f" ]
+then
+# https://drive.google.com/file/d/1exMdK7_qJdVlq_fh-FN_dB_XnO9pZQWM/view?usp=sharing
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies \
+  /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1exMdK7_qJdVlq_fh-FN_dB_XnO9pZQWM' \
+  -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1exMdK7_qJdVlq_fh-FN_dB_XnO9pZQWM" -O model.tar && rm -rf /tmp/cookies.txt
+
+tar -xvf model.tar
+mv ./model_6 $2
 else
 echo "invalid index"
 fi
